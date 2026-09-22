9254f807613d8a5898a16c98dd303f5fa23a17ff
samuel-kwasi-boahen <kwasisamuelboahen@gmail.com>
2026-09-22 10:35:04 +0000

    Add /menu route

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/health', (req, res) => {
   res.send('TaskFlow API is alive!');
 });
 
+app.get('/menu', (req, res) => {
+  res.send("Today's menu: rice, salad, chicken");
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
